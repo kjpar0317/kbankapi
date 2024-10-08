b2bc14a4900943a5dc2fcbf3da07831f8187c92b
박광주 <kjpar0317@innogrid.com>
2024-10-08 15:48:06 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import { P5CanvasInstance } from '@p5-wrapper/react';
 import { Ribbon as BackgroundRibbon } from './libs/background/ribbon';
 
-export function Ribbon() {
+export function Ribbon(p5: P5CanvasInstance) {
   if ('development' === process.env.NODE_ENV) {
     console.log('development');
   }
-  return BackgroundRibbon;
+  return new BackgroundRibbon(p5);
 }
